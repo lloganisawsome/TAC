09aa91513c8a7ba36b8c7e6df4ec191cb851dc8f
lloganisawsome <loganstatham76@gmail.com>
2026-02-13 10:18:47 -0600

    i guess

--- guard.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  try {
-    if (window.self === window.top) {
-      window.location.replace("https://www.google.com/");
-    }
-  } catch (e) {
-    window.location.replace("https://www.google.com/");
-  }
-})();
