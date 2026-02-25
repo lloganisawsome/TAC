039aadcfd469f60c9e0b98bc93914149ab083020
lloganisawsome <loganstatham76@gmail.com>
2026-02-25 11:27:38 -0600

    bum

---
+++ games/nintendo-64/dist/romlist.js
@@ -0,0 +1,14 @@
+var ROMLIST = [
+    {url:"roms/Top Demo (PD).z64",title:"Tom Demo"},
+    {url:"roms/TopGun Demo by Horizon64 (PD).z64",title:"TopGun Demo"},
+    {url:"roms/TR64 Demo by FIres and Icepir8 (PD).z64",title:"Game 3"},
+    {url:"roms/Tristar and Lightforce Quake Intro by Ayatollah & Mike (PD).z64",title:"Tristar and Lightforce Quake Intro"},
+    {url:"roms/TRON Demo (PD).z64",title:"TRON Demo"},
+    {url:"roms/TRSI Intro by Ayatollah(POM '98) (PD).z64",title:"TRSI Intro"},
+    {url:"roms/Twintris by Twinsen (POM '98) (PD).z64",title:"Twintris"},
+    {url:"roms/The Legend of Zelda - Ocarina of Time (USA).z64",title:"Legend of Zelda: Ocarina of Time"},
+    {url:"roms/Mario Kart 64 (E) (V1.1) [!].z64",title:"Mario Kart 64"},
+    {url:"roms/Legend of Zelda, The - Majora's Mask (U) [!].z64",title:"Legend of Zelda: Majora's Mask"},
+    {url:"roms/Pokemon Stadium (E) (V1.1) [!].z64",title:"Pokemon Stadium 1"},
+    {url:"roms/Pokemon Stadium 2 (U) [!].z64",title:"Pokemon Stadium 2"}
+];
